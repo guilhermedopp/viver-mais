4200de1d240ef6aa1d97e946c27e4301ab2aca7f
Guilherme Pontes <guilhermepontespinto@gmail.com>
2026-06-12 23:47:51 -0300

    Feat (Backend): Adiciona o campo 'visto' ao PostVO para futuro filtro de feed

--- viver-backend/src/main/java/com/vo/PostVO.java
+++ viver-backend/src/main/java/com/vo/PostVO.java
@@ -7,16 +7,22 @@ public class PostVO {
     private String conteudo;
     private Date data;
     private UsuarioVO autor;
+    private boolean visto; // Nova variável para filtro de Feed
 
     public PostVO(int id, String conteudo, UsuarioVO autor) {
         this.id = id;
         this.conteudo = conteudo;
         this.autor = autor;
         this.data = new Date();
+        this.visto = false; // Por padrão, um post novo nunca foi visto
     }
 
     public void setData(Date data){
         this.data = data;
+    }
+    
+    public void setVisto(boolean visto) {
+        this.visto = visto;
     }
 
     // Getters
@@ -24,4 +30,5 @@ public class PostVO {
     public UsuarioVO getAutor() { return autor; }
     public Date getData() { return data; }
     public int getId() { return id; }
+    public boolean isVisto() { return visto; }
 }
