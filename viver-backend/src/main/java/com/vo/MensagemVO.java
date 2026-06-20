9373bb984714da28f25002798fb93243e7a3433c
Guilherme Pontes <guilhermepontespinto@gmail.com>
2026-06-20 19:30:01 -0300

    refactor(backend): aplica heranca de MensagemBase e polimorfismo em MensagemVO

--- viver-backend/src/main/java/com/vo/MensagemVO.java
+++ viver-backend/src/main/java/com/vo/MensagemVO.java
@@ -2,37 +2,28 @@ package com.vo;
 
 import java.util.Date;
 
-public class MensagemVO {
-    private int id;
+public class MensagemVO extends MensagemBase {
     private UsuarioVO remetente;
     private UsuarioVO destinatario;
-    private String conteudo;
     private boolean lida;
-    private Date dataCriacao;
 
     public MensagemVO() {}
 
-    public MensagemVO(int id, UsuarioVO remetente, UsuarioVO destinatario, String conteudo, boolean lida, Date dataCriacao) {
-        this.id = id;
+    public MensagemVO(int id, UsuarioVO remetente, UsuarioVO destinatario,
+                      String conteudo, boolean lida, Date dataCriacao) {
+        super(id, conteudo, remetente, dataCriacao);
         this.remetente = remetente;
         this.destinatario = destinatario;
-        this.conteudo = conteudo;
         this.lida = lida;
-        this.dataCriacao = dataCriacao;
     }
 
-    public int getId()                   { return id; }
-    public UsuarioVO getRemetente()       { return remetente; }
-    public UsuarioVO getDestinatario()    { return destinatario; }
-    public String getConteudo()           { return conteudo; }
-    public boolean isLida()               { return lida; }
-    public Date getDataCriacao()          { return dataCriacao; }
+    @Override
+    public String getTipo() { return "PRIVADA"; }
 
-    
-    public void setId(int id) { this.id = id; }
-    public void setRemetente(UsuarioVO remetente) { this.remetente = remetente; }
-    public void setDestinatario(UsuarioVO destinatario) { this.destinatario = destinatario; }
-    public void setConteudo(String conteudo) { this.conteudo = conteudo; }
-    public void setLida(boolean lida) { this.lida = lida; }
-    public void setDataCriacao(Date dataCriacao) { this.dataCriacao = dataCriacao; }
+    public UsuarioVO getRemetente()            { return remetente; }
+    public UsuarioVO getDestinatario()         { return destinatario; }
+    public boolean isLida()                    { return lida; }
+    public void setRemetente(UsuarioVO r)      { this.remetente = r; }
+    public void setDestinatario(UsuarioVO d)   { this.destinatario = d; }
+    public void setLida(boolean l)             { this.lida = l; }
 }
