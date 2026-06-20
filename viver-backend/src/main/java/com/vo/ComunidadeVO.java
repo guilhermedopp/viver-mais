a93b115677ec729c6ef1bf6c032ec7ceca1b6dc9
Guilherme Pontes <guilhermepontespinto@gmail.com>
2026-06-20 19:25:58 -0300

    refactor(backend): implementa metodo abstrato getTipo em ComunidadeVO aplicando polimorfismo

--- viver-backend/src/main/java/com/vo/ComunidadeVO.java
+++ viver-backend/src/main/java/com/vo/ComunidadeVO.java
@@ -3,15 +3,17 @@ package com.vo;
 public class ComunidadeVO extends Perfil {
     private String descricao;
 
-    public ComunidadeVO() {
-        super();
-    }
+    public ComunidadeVO() { super(); }
 
     public ComunidadeVO(int id, String nome, String descricao) {
         super(id, nome);
         this.descricao = descricao;
     }
 
-    public String getDescricao() { return descricao; }
-    public void setDescricao(String descricao) { this.descricao = descricao; }
+    // Polimorfismo: ComunidadeVO diz que é do tipo "COMUNIDADE"
+    @Override
+    public String getTipo() { return "COMUNIDADE"; }
+
+    public String getDescricao()               { return descricao; }
+    public void   setDescricao(String d)       { this.descricao = d; }
 }
